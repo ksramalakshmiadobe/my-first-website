3bc8be4543f837be5b32b9f8e5c0023106283e1b
Ramalakshmi K s <ramalakshmi@adobe.com>
2024-05-31 13:42:36 +0530

    saving changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,36 +1,11 @@
-import { createOptimizedPicture, fetchPlaceholders } from '../../scripts/aem.js';
+import { createOptimizedPicture, fetchPlaceholders, decorateButtons } from '../../scripts/aem.js';
 
 // fetch placeholders from the 'en' folder
 const placeholders = await fetchPlaceholders('en');
 // retrieve the value for key 'foo'
 const { click } = placeholders;
 
-export function decorateButtons(element) {
-  element.querySelectorAll('a').forEach((a) => {
-    a.title = a.title || a.textContent;
-    a.innerHTML = click;
-    if (a.href !== a.textContent) {
-      const up = a.parentElement;
-      const twoup = a.parentElement.parentElement;
-      if (!a.querySelector('img')) {
-        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-          a.className = 'button primary'; // default
-          up.classList.add('button-container');
-        }
-        if (up.childNodes.length === 1 && up.tagName === 'STRONG'
-            && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button primary';
-          twoup.classList.add('button-container');
-        }
-        if (up.childNodes.length === 1 && up.tagName === 'EM'
-            && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button secondary';
-          twoup.classList.add('button-container');
-        }
-      }
-    }
-  });
-}
+decorateButtons()
 
 export default function decorate(block) {
   /* change to ul, li */
