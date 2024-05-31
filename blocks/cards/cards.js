fdd3a0e713bbcdbfbbed672f98d17f315e5cbf4e
Ramalakshmi K s <ramalakshmi@adobe.com>
2024-05-31 12:27:03 +0530

    saving changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -24,7 +24,8 @@ export default function decorate(block) {
 
 export function decorateButtons(element) {
   element.querySelectorAll('a').forEach((a) => {
-    a.textContent = click;
+    a.title = a.title || a.textContent;
+    a.innerHTML = click;
     // console.log('a title:', a.title);
     // console.log('a textcontent:', a.textContent);
     // a.textContent = click;
